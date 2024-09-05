4fbfc70c572dbb6d79c28515a7fd3a6fa47000fd
jeeshian <jeeshian2001@gmail.com>
2024-09-05 22:44:45 +0800

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+dotenv.config();
 
 const app = express();
 
@@ -6,7 +9,10 @@ app.get('/products', (req, res) => {
     res.send('Hello World!');
 });
 
+
 app.listen(5000, () => {
+    connectDB();
     console.log('server started at http://localhost:5000 ');
 }
 )
+
