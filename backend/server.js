5c20d95b37d1dd74bfb56b7d4d4da7761317562c
jeeshian <jeeshian2001@gmail.com>
2024-09-06 15:39:26 +0800

    done DELETE request for deleting product in database

--- backend/server.js
+++ backend/server.js
@@ -26,6 +26,19 @@ app.post('/api/products', async (req, res) => {
     }
 });
 
+// delete product from database
+app.delete('/api/products/:id', async (req, res) => {
+    const {id} = req.params; // get the id of the product to be deleted
+    console.log("id:" + id); //show the id
+
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "product is deleted successfully"});
+    } catch (error) {
+        res.status(404).json({ success: false, message: 'Product not found.' });
+    }
+});
+
 
 app.listen(5000, () => {
     connectDB();
