4e1ba525b2816010424cc84fe5c8a07d879e8a22
jeeshian <jeeshian2001@gmail.com>
2024-09-09 13:22:27 +0800

    DeleteProduct--integrated frontend and backend

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,13 +1,40 @@
 import { DeleteIcon, EditIcon } from '@chakra-ui/icons'
-import { Box, Heading, HStack, IconButton, Image, Text, useColorModeValue} from '@chakra-ui/react'
+import { Box, Heading, HStack, IconButton, Image, Text, useColorModeValue, useToast} from '@chakra-ui/react'
 import React from 'react'
+import { useProductStore } from '../store/product';
 
 const ProductCard = ({product}) => {
   const textColor = useColorModeValue('gray.600', 'gray.200');
   const bg = useColorModeValue('white', 'gray.800');
+
+  const {deleteProduct} = useProductStore();
+  const toast = useToast();
+
+  const handleDeleteProduct = async (productId) => {
+    const {success, message} = await deleteProduct(productId);
+    console.log(success, message);
+    if (!success) {
+      toast({
+        title: 'Error',
+        description: message,
+        status: 'error',
+        duration: 3000,
+        isClosable: true,
+      })
+    } else{
+      toast({
+        title: 'Success',
+        description: message,
+        status:'success',
+        duration: 3000,
+        isClosable: true,
+      })
+    }
+  }
+
   return (
     <Box shadow={'lg'} rounded={'lg'} overflow={'hidden'} transition={'all 0.3s'}
-    _hover={{transform: 'translate(-5px)', shadow: 'xl'}} bg={bg}
+    _hover={{transform: 'translateY(-5px)', shadow: 'xl'}} bg={bg}
     >
         <Image src={product.image} alt={product.name} h={48} w={'full'} objectFit={'cover'} />
 
@@ -18,7 +45,7 @@ const ProductCard = ({product}) => {
 
             <HStack spacing={2}>
                 <IconButton icon={<EditIcon/>} colorScheme='blue' ></IconButton>
-                <IconButton icon={<DeleteIcon/>} colorScheme='red' ></IconButton>
+                <IconButton icon={<DeleteIcon/>} colorScheme='red' onClick={()=>handleDeleteProduct(product._id)}></IconButton>
             </HStack>
         </Box>
     </Box>
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -23,5 +23,18 @@ export const useProductStore = create((set) => ({
         const res = await fetch('/api/products');
         const data = await res.json();
         set({products: data.data});
+    },
+    deleteProduct: async (productId) => {
+        const res = await fetch(`/api/products/${productId}`, {
+            method: 'DELETE'
+        });
+        const data = await res.json(); //response from backend
+        console.log('product js:', data.success, data.message);
+        if(!data.success){
+            return {success:false, message:data.message}
+        }
+        set((state) => ({products: state.products.filter((product) => product._id!== productId)}));
+        return {success: true, message: data.message};
+        
     }
 }));
