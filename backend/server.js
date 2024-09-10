a60e595263647af558e63a735c5add03515b831e
jeeshian <jeeshian2001@gmail.com>
2024-09-10 12:39:38 +0800

    combined the client domain and server domain into 1 single domain

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
 import productRoutes from './routes/product.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -12,6 +13,19 @@ app.use(express.json()); // allows us to accept JSON data in the req.body
 
 app.use("/api/products", productRoutes);
 
+const __dirname = path.resolve();
+console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('NODE_ENV bool:', process.env.NODE_ENV === "production");
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+    console.log('static assets')
+    app.use(express.static(path.join(__dirname, '/frontend/dist'))); 
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
+    });
+}
+console.log('not static assets')
+console.log('NODE_ENV:', process.env.NODE_ENV);
 
 app.listen(PORT, () => {
     connectDB();
